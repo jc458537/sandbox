36c0844eb2b0c5b10d798d8a846fe44cec0c9f1d
jc458537 <patel_bhumi2112@yahoo.in>
2016-12-09 20:01:01 +1000

    complete the file

--- oddname.py
+++ oddname.py
@@ -1,5 +1,15 @@
 __author__ = 'jc458537'
 """
-        Bhoomi Raval
+        Bhoomi
 """
+valid = False
+while not valid:
+    name = input("Enter your name:")
+    if name != "" and not name.isspace():
+            valid = True
 
+print(valid)
+
+l = len(name)
+for i in range(0, l, 2):
+        print(name[i])
